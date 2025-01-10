d1ceb9e9b07ec1fac408777f5ef7505205c96244
Alexdosantos <alexdosantoss@yahoo.com>
2025-01-10 03:47:17 -0300

    Chore: Testando o token

--- src/lib/axios/axiosCreate.tsx
+++ src/lib/axios/axiosCreate.tsx
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
+const TOKEN = "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"
+
 const api = axios.create({
   baseURL: 'https://api.github.com',
   headers: {
     Accept: 'application/vnd.github+json',
     'X-GitHub-Api-Version': '2022-11-28',
     'User-Agent': 'Petize-Github-App',
-    Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`
+    Authorization: `Bearer ${TOKEN}`,
   }
 });
 
